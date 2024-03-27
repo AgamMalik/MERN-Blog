ba11252a03a8292f89eafccabd3134fa84bae5ec
AgamMalik <agam.malik31@gmail.com>
2024-03-27 18:20:46 +0530

    updated header component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,11 @@
-import {Button, Navbar, TextInput} from "flowbite-react"
-import { Link, useLocation } from "react-router-dom"
-import {AiOutlineSearch} from "react-icons/ai"
-import {FaMoon} from "react-icons/fa"
-
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 export default function Header() {
-
+  const { currentUser } = useSelector((state) => state.user);
   const path = useLocation().pathname;
 
   return (
@@ -34,9 +34,35 @@ export default function Header() {
         <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
           <FaMoon />
         </Button>
-        <Link to="/sign-in">
-          <Button className="bg-teal-500" outline>Sign In</Button>
-        </Link>
+
+        {/* if we dont have any currentUser in state then signin button otherwise avatar */}
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item >Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
+            <Button className="bg-teal-500" outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
