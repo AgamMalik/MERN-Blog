d87006c55aae6a3370f69218ba8eaf08cab190c0
AgamMalik <agam.malik31@gmail.com>
2024-03-27 16:44:01 +0530

    added redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -15,12 +15,19 @@ const persistConfig = {
   version: 1,
 };
 
+// instead of having many reducers... we are using persist reducer
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+
+  // to prevent errors using redux toolkit
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export const persistor = persistStore(store);
+
+
+
+// Serializable Actions: Redux expects that actions dispatched to the store are plain JavaScript objects, and they should be serializable, meaning they can be converted into a string representation and then later restored to their original form. This is important for various reasons, including debugging, time-traveling debugging, and also for persisting state.
